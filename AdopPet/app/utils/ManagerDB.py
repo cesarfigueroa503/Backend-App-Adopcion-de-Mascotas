bff42ba9d138ab1cad5cb3f52b4be0a150004543
Cesar Banegas <133507275+cesarfigueroa503@users.noreply.github.com>
2023-08-16 03:28:09 -0600

    Update ManagerDB.py

--- AdopPet/app/utils/ManagerDB.py
+++ AdopPet/app/utils/ManagerDB.py
@@ -1,6 +1,6 @@
 from peewee import Model, PostgresqlDatabase
 
-db = PostgresqlDatabase('proyecto', user = "postgres", host = '192.168.0.11', port = 5432)
+db = PostgresqlDatabase('proyecto', user = "grupo1" , password= 123, host = '192.168.0.11', port = 5432)
 
 class BaseModel(Model):
     DB=db
